06786ab4805655185a343ca543b11cab6bab4d6d
WWW_cool <WWW_cool@inbox.ru>
2021-02-17 12:14:04 +0300

    added configurator service

---
+++ proto/configurator.thrift
@@ -0,0 +1,29 @@
+include "base.thrift"
+include "limiter.thrift"
+
+namespace java com.rbkmoney.limiter.configurator
+namespace erlang cfg
+
+typedef base.ID LimitName
+
+struct LimitCreateParams {
+    1: optional LimitName name
+}
+
+struct LimitNameNotFound {}
+
+exception InconsistentRequest {
+    1: optional LimitNameNotFound limit_name_not_found
+}
+
+service Configurator {
+    limiter.Limit Create(1: LimitCreateParams params) throws (
+        1: InconsistentRequest e1,
+        2: base.InvalidRequest e2
+    )
+
+    limiter.Limit Get(1: limiter.LimitID id, 2: base.Timestamp timestamp) throws (
+        1: limiter.LimitNotFound e1,
+        2: base.InvalidRequest e2
+    )
+}
